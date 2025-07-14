c218d7b6dc5b7ebf0fc45b4f3b8b2bf4629b8044
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2025-07-13 22:52:15 -0700

    make vercel work challenge (impossible)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   server: {
     allowedHosts: true
   },
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
